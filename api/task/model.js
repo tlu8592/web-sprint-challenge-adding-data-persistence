83f4f1dd1b91d8c71567dcda8fc10eb6948922ed
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-11 02:15:03 -0400

    wrote task model functions

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,20 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig');
+
+function getTasks() {
+    return db('tasks').truncate();
+}
+
+function getTaskById(task_id) {
+    return db('tasks').where('task_id', task_id).first();
+}
+
+function addNewTask(task) {
+    return db('tasks').insert(task)
+        .then(([task_id]) => getTaskById(task_id));
+}
+
+module.exports = {
+    getTasks,
+    addNewTask
+}
