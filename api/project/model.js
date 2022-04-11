ffce976e6c88e6088a175e8f629dc66dc35ec60c
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-11 01:32:01 -0400

    wrote project model functions

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,23 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig');
+
+function getProjects() {
+    return db('projects')
+}
+
+function getProjectById(project_id) {
+    return db('projects')
+        .where('project_id', project_id)
+        .first();
+}
+
+function addNewProject(project) {
+    return db('projects').insert(project)
+        .then(([project_id]) => getProjectById(project_id));
+}
+
+module.exports = {
+    getProjects,
+    getProjectById,
+    addNewProject
+}
