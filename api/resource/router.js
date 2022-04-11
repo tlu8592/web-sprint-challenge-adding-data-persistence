f7747e8102a7d5eec33dbb501cd177f388731db1
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-11 01:09:12 -0400

    wrote endpoints for resource router

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,4 +1,41 @@
 // build your `/api/resources` router here
 const router = require('express').Router();
+const Resource = require('./model');
+const db = require('../../data/dbConfig');
+
+router.get('/', (req, res) => {
+    Resource.getResources()
+        .then(resources => {
+            res.json(resources);
+        })
+        .catch(error => {
+            res.status(404).json({
+                message: "no resources"
+            });
+        })
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const existing = await db('resources')
+            .where('resource_name', req.body.resource_name)
+            .first();
+        if (existing) {
+            res.status(400).json({
+                message: "resource name already exists"
+            });
+        } else {
+            const newResource = await Resource.addNewResource({
+                resource_name: req.body.resource_name,
+                resource_description: req.body.resource_description
+            });
+            res.status(201).json(newResource);
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "error creating resource"
+        });
+    }
+})
 
 module.exports = router;
