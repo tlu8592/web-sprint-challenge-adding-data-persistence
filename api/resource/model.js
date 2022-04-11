4fe33485ca3d0dd9ce1b1dbee9a389a1737b9593
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-11 01:08:24 -0400

    wrote model functions

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,21 @@
 // build your `Resource` model here
+const db = require('../../data/dbConfig');
+
+function getResources() {
+    return db('resources');
+}
+
+function getResourceById(resource_id) {
+    return db('resources').where('resource_id', resource_id)
+}
+
+function addNewResource(resource) {
+    return db('resources').insert(resource)
+        .then(([resource_id]) => getResourceById(resource_id))
+}
+
+module.exports = {
+    getResources,
+    getResourceById,
+    addNewResource
+}
