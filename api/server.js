daf7dc415c3a3c85e01cb17e10afeef95e1dd915
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-11 01:09:40 -0400

    initial setup

--- api/server.js
+++ api/server.js
@@ -8,8 +8,8 @@ const server = express();
 
 server.use(express.json());
 
-server.use('/api/project', projectRouter);
-server.use('/api/resource', resourceRouter);
-server.use('/api/task', taskRouter);
+server.use('/api/projects', projectRouter);
+server.use('/api/resources', resourceRouter);
+server.use('/api/tasks', taskRouter);
 
 module.exports = server;
